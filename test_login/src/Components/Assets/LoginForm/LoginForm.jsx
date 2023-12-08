96dc619f3411a16fc0ee042ddc6bb62c0f137dec
brunoedu <brunoedusouza17@gmail.com>
2023-12-08 14:15:46 -0300

    first

--- test_login/src/Components/Assets/LoginForm/LoginForm.jsx
+++ test_login/src/Components/Assets/LoginForm/LoginForm.jsx
@@ -3,36 +3,74 @@ import './LoginForm.css';
 import { FaUser, FaLock } from "react-icons/fa";
 
 
+const estBr = [
+    { sigla: 'AC', nome: 'Acre' },
+    { sigla: 'AL', nome: 'Alagoas' },
+    { sigla: 'AP', nome: 'Amapá' },
+    { sigla: 'AM', nome: 'Amazonas' },
+    { sigla: 'BA', nome: 'Bahia' },
+    { sigla: 'CE', nome: 'Ceará' },
+    { sigla: 'DF', nome: 'Distrito Federal' },
+    { sigla: 'ES', nome: 'Espírito Santo' },
+    { sigla: 'GO', nome: 'Goiás' },
+    { sigla: 'MA', nome: 'Maranhão' },
+    { sigla: 'MT', nome: 'Mato Grosso' },
+    { sigla: 'MS', nome: 'Mato Grosso do Sul' },
+    { sigla: 'MG', nome: 'Minas Gerais' },
+    { sigla: 'PA', nome: 'Pará' },
+    { sigla: 'PB', nome: 'Paraíba' },
+    { sigla: 'PR', nome: 'Paraná' },
+    { sigla: 'PE', nome: 'Pernambuco' },
+    { sigla: 'PI', nome: 'Piauí' },
+    { sigla: 'RJ', nome: 'Rio de Janeiro' },
+    { sigla: 'RN', nome: 'Rio Grande do Norte' },
+    { sigla: 'RS', nome: 'Rio Grande do Sul' },
+    { sigla: 'RO', nome: 'Rondônia' },
+    { sigla: 'RR', nome: 'Roraima' },
+    { sigla: 'SC', nome: 'Santa Catarina' },
+    { sigla: 'SP', nome: 'São Paulo' },
+    { sigla: 'SE', nome: 'Sergipe' },
+    { sigla: 'TO', nome: 'Tocantins' },
+];
+
 
 const LoginForm = () => {
 
     return (
-        <div className="conteudo">
-            <form action="">
-            <h1>Login</h1>
 
-            <div className="input-box">
-                <input type="text" placeholder="Username" required />
-                <FaUser className='icon'/>
+        <div className="main">
+
+
+
+            <div className="inputs">
+
+
+                <div className="input_1">
+            <div className="select">
+                <label htmlFor="estados">Selecione um estado:</label>
+                <select id="estados" name="estados">
+                    {estBr.map((estado) => (
+                        <option key={estado.sigla} value={estado.sigla}>
+                            {estado.nome}
+                        </option>
+                    ))}
+                </select>
             </div>
-
-            <div className="input-box">
-                <input type="password" placeholder="Password" required />
-                <FaLock className='icon'/>
-            </div>
-
-            <div className="recuperar">
-                <label> <input type="checkbox" /> Remeber me </label>
-                <a href="#"> Forgot password?</a>
-            </div>
-
-                <button className="btn" type="submit">Login</button>
-
-                <div className="link-registro">
-                    <p>Nao tem uma conta ?  <a href="#">Registre-se</a></p>
+                    <input className="inp"  type="text" placeholder="nome" />
+                    <input className="inp"  type="date" placeholder="Data Nascimento" />
+                    <input className="inp"  type="text" placeholder="Celular" />
+                    <input className="inp"  type="text" placeholder="Endereço" />
                 </div>
 
-            </form>
+                <div className="input_2">
+
+                    <input className="inp1"  type="email" placeholder="E-mail" />
+                    <input className="inp"  type="text" placeholder="Naturalidade" />
+                    <input className="inp"  type="tell" placeholder="Celular" />
+                    <input className="inp"  type="text" placeholder="Endereço" />
+                </div>
+
+            </div>
         </div>
     );
 };
